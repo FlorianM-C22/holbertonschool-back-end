6e412b781c19040de50cb4d6a10b218726ac7a10
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-15 13:27:10 +0200

    Task 2 - Testing checker

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""Script that saves infos from a given employee into a .csv file"""
-
+"""Export data from a REST API to a JSON file"""
 import json
 import requests
-from sys import argv
+from sys import argv, exit
 
-API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == '__main__':
-    user_id = argv[1]
-    response = requests.get(f"{API_URL}/users/{user_id}/todos",
-                            params={"_expand": "user"})
+if __name__ == "__main__":
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = argv[1]
+
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
     data = response.json()
 
     if not len(data):
         print("RequestError:", 404)
         exit(1)
 
-    user_tasks = {user_id: []}
+    user_tasks = {EMPLOYEE_ID: []}
     for task in data:
         task_dict = {
             "task": task["title"],
             "completed": task["completed"],
             "username": task["user"]["username"]
         }
-        user_tasks[user_id].append(task_dict)
+        user_tasks[EMPLOYEE_ID].append(task_dict)
 
-    # Write data to a JSON file
-    with open(f"{user_id}.json", mode='w') as json_file:
-        json.dump(data, json_file, indent=4)
+    with open(f"{EMPLOYEE_ID}.json", "w") as file:
+        json.dump(user_tasks, file)
