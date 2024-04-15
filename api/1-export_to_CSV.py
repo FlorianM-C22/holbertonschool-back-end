1fce03a44b0ec2a6cd486f7f445af21360d14c94
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-15 13:02:19 +0200

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns information for a given employee ID using a REST API"""
+"""Saves informations from a given user ID to a .csv file"""
 
 import requests
 import csv
@@ -8,19 +8,12 @@ from sys import argv
 API_URL = "https://jsonplaceholder.typicode.com/"
 
 if __name__ == "__main__":
-    # Retrieve user data
-    user_id = argv[1]
-    user_response = requests.get(f'{API_URL}/users/{user_id}')
-    user = user_response.json()
+    # Get user and tasks informations
+    user = requests.get(f'{API_URL}/users/{argv[1]}').json()
+    todo_list = requests.get(f"{API_URL}/todos?userId={argv[1]}").json()
 
-    # Retrieve todo list
-    todo_response = requests.get(f"{API_URL}/todos?userId={user_id}")
-    todo_list = todo_response.json()
-
-    # Create CSV file
-    csv_filename = f"{user_id}.csv"
-
-    with open(csv_filename, mode="w") as csv_file:
+    # Open a new .csv file
+    with open(f"{argv[1]}.csv", mode="w") as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         # Write into the csv file
