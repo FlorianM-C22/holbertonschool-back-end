d5ff7e79b8b84755f04756203c9ff996c8be79a4
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-15 13:03:25 +0200

    Task 1 - More corrections

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Saves informations from a given user ID to a .csv file"""
+"""Script that saves infos from a given employee into a .csv file"""
 
-import requests
 import csv
+import requests
 from sys import argv
 
-API_URL = "https://jsonplaceholder.typicode.com/"
+API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == "__main__":
-    # Get user and tasks informations
+if __name__ == '__main__':
     user = requests.get(f'{API_URL}/users/{argv[1]}').json()
     todo_list = requests.get(f"{API_URL}/todos?userId={argv[1]}").json()
 
-    # Open a new .csv file
-    with open(f"{argv[1]}.csv", mode="w") as csv_file:
+    with open(f"{argv[1]}.csv", mode='w') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-        # Write into the csv file
         for task in todo_list:
             csv_writer.writerow([
                 user['id'],
@@ -24,3 +21,5 @@ if __name__ == "__main__":
                 task['completed'],
                 task['title']
             ])
+
+    print(f"Data has been exported to {argv[1]}.csv")
